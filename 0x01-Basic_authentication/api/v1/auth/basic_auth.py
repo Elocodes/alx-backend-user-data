c14ddb73f2ed084d6ee732c4d4c63b572fbb877f
Elocodes <elochukwuodoeze@gmail.com>
2024-01-19 15:09:44 -0800

    added basic_auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module contains the basic authentication systems
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ basic authentication class """
+    pass
