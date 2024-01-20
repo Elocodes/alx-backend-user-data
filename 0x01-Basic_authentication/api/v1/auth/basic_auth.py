8ac90f623dd588ecd2a7f7911d1671081161939a
Elocodes <elochukwuodoeze@gmail.com>
2024-01-19 20:12:08 -0800

    decode b64 func

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ module contains the basic authentication systems
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,4 +21,18 @@ class BasicAuth(Auth):
         if auth_header.startswith('Basic '):
             return auth_header.split(' ')[1]
         else:
+            return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """
+        returns the decoded value of a Base64 string
+        """
+        auth_header = base64_authorization_header
+        if (auth_header is None) or (type(auth_header) is not str):
+            return None
+        try:
+            base = base64.b64decode(auth_header)
+            return base.decode('utf-8')
+        except Exception:
             return None
