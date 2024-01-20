7fd1bcb568d92449f0fe04aa2f10b5409abd1ff4
Elocodes <elochukwuodoeze@gmail.com>
2024-01-19 20:51:58 -0800

    user obj func

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ module contains the basic authentication systems
 
 from api.v1.auth.auth import Auth
 import base64
+from flask import request
+from typing import List, TypeVar
 
 
 class BasicAuth(Auth):
@@ -51,3 +53,33 @@ class BasicAuth(Auth):
             return (email, password)
         else:
             return (None, None)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        returns the User instance based on his email and password.
+        """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users_directory = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users_directory:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """retrieves users instance of a request"""
+        header_auth = request.headers
+        if header_auth is None:
+            return None
+        ext_base = self.extract_base64_authorization_header(header_auth)
+        decode_base = self.decode_base64_authorization_header(ext_base)
+        user_cred = self.extract_user_credentials(decode_base)
+        email = user_cred[0]
+        pwd = user_cred[1]
+        return self.user_object_from_credentials(email, pwd)
