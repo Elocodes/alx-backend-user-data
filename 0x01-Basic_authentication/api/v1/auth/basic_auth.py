34bbf5a41108e3c243ed8243f0c35043e0bd3317
Elocodes <elochukwuodoeze@gmail.com>
2024-01-19 19:50:51 -0800

    added func that returns Base64 part for basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ basic authentication class """
-    pass
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
+        """
+        returns the Base64 part of the Authorization header for a
+        Basic Authentication
+        """
+        auth_header = authorization_header
+        if (auth_header is None) or (type(auth_header) is not str):
+            return None
+        if auth_header.startswith('Basic '):
+            return auth_header.split(' ')[1]
+        else:
+            return None
