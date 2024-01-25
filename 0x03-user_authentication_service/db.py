81291f98eb491e8bc931ae19e65ca6e66e63a663
Elocodes <elochukwuodoeze@gmail.com>
2024-01-24 19:08:17 -0800

    set reset token to Nine

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,8 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ save user to database """
-        new_user = User(email=email, hashed_password=hashed_password)
+        new_user = User(email=email, hashed_password=hashed_password,
+                        session_id=None, reset_token=None)
         self._session.add(new_user)
         self._session.commit()
         return new_user
