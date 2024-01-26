67e544c1b895eaee661050bbf674844be2e3d294
Elocodes <elochukwuodoeze@gmail.com>
2024-01-25 19:39:51 -0800

    create new user func

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,9 +5,11 @@ hash password
 
 import bcrypt
 from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password):
+def _hash_password(password: str) -> str:
     """ method takes in a password string, hashes it,
     and returns bytes
     """
@@ -27,3 +29,13 @@ class Auth:
     def __init__(self):
         """ init """
         self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register new user """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -39,7 +39,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
         method takes in arbitrary keyword arguments and returns
         the first row found in the users table as filtered by
